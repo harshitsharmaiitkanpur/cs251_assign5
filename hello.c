227449f6673a42d9c23404e063fe74164c567aa5
harshit sharma <harshitsharma2307@gmail.com>
2018-03-04 10:10:53 +0530

    hello.c modified

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
  void microkernel_sendmsg(char *);
-
+ void bro(){
+  printf("changes are made");
+ }
  void main(){
  printf("Helloworld!\n");
  printf("This must be a monolithic design\n");
